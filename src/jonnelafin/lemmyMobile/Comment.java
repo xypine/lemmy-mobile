b71c069f8663bacef7a28dcd72607014da4039dc
Elias Eskelinen <elias.eskelinen@protonmail.com>
2020-06-09 18:02:06 +0300

    comments

---
+++ src/jonnelafin/lemmyMobile/Comment.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jonnelafin.lemmyMobile;
+
+import com.codename1.charts.util.ColorUtil;
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Container;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.Font;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.URLImage;
+import com.codename1.ui.geom.Dimension;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.Layout;
+import com.codename1.ui.plaf.Style;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import static jonnelafin.lemmyMobile.LemmyMain.cardCount;
+import static jonnelafin.lemmyMobile.LemmyMain.commentView;
+import static jonnelafin.lemmyMobile.LemmyMain.curr;
+
+/**
+ *
+ * @author Elias Eskelinen <elias.eskelinen@protonmail.com>
+ */
+public class Comment extends Container{
+    LinkedHashMap raw;
+    Label creator_avatar;
+    String creator_name;
+    String content;
+    int parent;
+    int id;
+    int upvotes;
+    int downvotes;
+    int rank;
+    
+    public LinkedList<Comment> children = new LinkedList<>();
+    
+
+    public Comment(LinkedHashMap raw) {
+        this.raw = raw;
+        Layout l = new BorderLayout();
+        this.setLayout(l);
+        Container c = this;
+        c.setWidth(commentView.getWidth());
+        
+        creator_name = (String) raw.get("creator_name");
+        content = (String) raw.get("content");
+        
+        parent = -1;
+        try {
+            parent = (int) Math.round((double) raw.get("parent_id"));
+        } catch (Exception e) {
+        }
+        id = -1;
+        try {
+            id = (int) Math.round((double) raw.get("id"));
+        } catch (Exception e) {
+        }
+        rank = -1;
+        try {
+            rank = (int) Math.round((double) raw.get("score"));
+        } catch (Exception e) {
+        }
+        upvotes = -1;
+        try {
+            rank = (int) Math.round((double) raw.get("upvotes"));
+        } catch (Exception e) {
+        }
+        downvotes = -1;
+        try {
+            rank = (int) Math.round((double) raw.get("downvotes"));
+        } catch (Exception e) {
+        }
+        
+        
+        
+        c.setUIID(id + "");
+        c.setName(rank + "");
+        if(parent != -1){
+            c.setUIID(parent + "_" + id);
+        }
+        c.setFocusable(true);
+        //c.setDraggable(true);
+        //c.setHeight(1050);
+        c.setTactileTouch(true);
+        c.setRippleEffect(true);
+        curr = raw;
+        c.addPointerReleasedListener((evt) -> {
+            //showPost(raw);
+        });
+        
+        int bg = ColorUtil.rgb(0, 0, 0);
+        if(true){
+            bg = ColorUtil.rgb(25, 25, 25);
+        }
+        int fg = ColorUtil.rgb(255, 255, 255);
+        Style card_style = new Style(fg, bg, Font.getDefaultFont(), Byte.MAX_VALUE);
+        c.setSelectedStyle(card_style);
+        c.setUnselectedStyle(card_style);
+        
+        creator_avatar = new Label("");
+        creator_avatar.setSelectedStyle(card_style);
+        creator_avatar.setUnselectedStyle(card_style);
+            String imgUrl = (String) raw.get("creator_avatar");
+            //System.out.println(imgUrl);
+            try {
+                EncodedImage p = EncodedImage.createFromImage(Image.createImage(100, 100, 0xffff0000), true);
+                URLImage img = URLImage.createToStorage(p, "/comment_" + cardCount + "profile", imgUrl);
+                creator_avatar.setIcon(img);
+            } catch (Exception e) {
+                FontImage.setMaterialIcon(creator_avatar, FontImage.MATERIAL_PERSON);
+            }
+        //c.add(BorderLayout.WEST, new SpanLabel(FontImage.MATERIAL_LINK+""));
+        creator_avatar.setSize(new Dimension(300, 300));
+        c.add(BorderLayout.WEST, creator_avatar);
+        
+        Label owner = new Label(creator_name);
+        owner.setUnselectedStyle(card_style);
+        owner.setSelectedStyle(card_style);
+        c.add(BorderLayout.NORTH, owner);
+        
+        SpanLabel name = new SpanLabel(content);
+        name.setUnselectedStyle(card_style);
+        name.setSelectedStyle(card_style);
+        c.add(BorderLayout.CENTER, name);
+        
+        c.add(BorderLayout.EAST, new Label("                    "));
+    }
+    
+}
--- src/jonnelafin/lemmyMobile/Parser.java
+++ src/jonnelafin/lemmyMobile/Parser.java
@@ -8,9 +8,11 @@ package jonnelafin.lemmyMobile;
 import com.codename1.charts.util.ColorUtil;
 import com.codename1.components.SpanLabel;
 import com.codename1.ui.Container;
+import com.codename1.ui.Display;
 import com.codename1.ui.Font;
 import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.plaf.Style;
+import java.util.LinkedList;
 
 /**
  *
@@ -19,19 +21,38 @@ import com.codename1.ui.plaf.Style;
 public class Parser {
     
     //This was a bad idea.
-    
+    //[Later] Well, it seems as the browser doesn't work too well. I'll try this again.
     
     
     public static void main(String[] args) {
-        String line = "####.--";
+        String line = "Hi! [Here is a link](https://test.link) see, if it works! \n[here](is another) EOF";
         System.out.println(startsWith(line, "####."));
         
         
-        int pos = line.indexOf(".");
-        int pos2 = line.indexOf("]");
-        String before = line.substring(0, pos);
-        String after = line.substring(pos+1);
-        line = before + after;
+        
+        while(line.contains("[")){
+            int pos = line.indexOf("[");
+            int pos2 = line.indexOf("]");
+            String before = line.substring(0, pos);
+            String after = line.substring(pos+1);
+            if(pos2 == -1){
+                line = before + after;
+            }
+            else{
+                String title = line.substring(pos, pos2);
+                int pos3 = line.indexOf("(");
+                int pos4 = line.indexOf(")");
+                if(pos3 != -1 && pos4 != -1){
+                    String url = line.substring(pos3, pos4);
+                    //links.add(new link(pos, title, url));
+                    after = line.substring(pos4+1);
+                    line = before + after;
+                }
+                else{
+                    line = before + after;
+                }
+            }
+        }
         System.out.println(line);
         
     }
@@ -40,6 +61,7 @@ public class Parser {
         FlowLayout layout = new FlowLayout();
         Container body = new Container(layout);
         for(String line : splitByDelimiter(post2, "\n")){
+            LinkedList<link> links = new LinkedList<>();
             String op = line + "";
             
             //Links
@@ -55,9 +77,19 @@ public class Parser {
                     String title = line.substring(pos, pos2);
                     int pos3 = line.indexOf("(");
                     int pos4 = line.indexOf(")");
+                    if(pos3 != -1 && pos4 != -1){
+                        String url = line.substring(pos3, pos4);
+                        links.add(new link(pos, title, url));
+                        after = line.substring(pos4+1);
+                        line = before + after;
+                    }
+                    else{
+                        line = before + after;
+                    }
                 }
             }
             
+            /*
             //General style
             if(startsWith(line, "#")){
                 SpanLabel header = new SpanLabel(op.substring(1));
@@ -66,8 +98,63 @@ public class Parser {
                 Style card_style = new Style(fg, bg, Font.create("SYSTEM-PLAIN-LARGE"), Byte.MAX_VALUE);
                 header.setTextUnselectedStyle(card_style);
                 body.add(header);
+            }*/
+            int i = 0;
+            String buff = "";
+            for(char c : line.toCharArray()){
+                buff = buff + c;
+                boolean isLink = false;
+                for(link a : links){
+                    if(a.pos == i){
+                        isLink = true;
+                        SpanLabel txt = new SpanLabel(buff);
+                        body.add(txt);
+                        buff = "";
+                        
+                        int bg = ColorUtil.rgb(0, 0, 0);
+                        int fg = ColorUtil.rgb(25, 25, 255);
+                        Style card_style = new Style(fg, bg, Font.getDefaultFont(), Byte.MAX_VALUE);
+                        SpanLabel alink = new SpanLabel(a.title);
+                        alink.setUnselectedStyle(card_style);
+                        alink.addPointerReleasedListener((arg0) -> {
+                            Display.getInstance().execute(a.url);
+                        });
+                        body.add(alink);
+                    }
+                }
+                i++;
             }
-            
+            SpanLabel txt = new SpanLabel(buff);
+            body.add(txt);
+            /*
+            int last = 0;
+            int ind = 0;
+            for(link i : links){
+                boolean isLast = false;
+                try {
+                    link next = links.get(ind + 1);
+                } catch (Exception e) {
+                    isLast = true;
+                }
+                
+                String before = line.substring(last, i.pos);
+                body.add(new SpanLabel(before));
+                int bg = ColorUtil.rgb(0, 0, 0);
+                int fg = ColorUtil.rgb(25, 25, 255);
+                Style card_style = new Style(fg, bg, Font.getDefaultFont(), Byte.MAX_VALUE);
+                SpanLabel alink = new SpanLabel(i.title);
+                alink.setUnselectedStyle(card_style);
+                alink.addPointerReleasedListener((arg0) -> {
+                    Display.getInstance().execute(i.url);
+                });
+                body.add(alink);
+                if(isLast){
+                    String next = line.substring(i.pos);
+                    body.add(new SpanLabel(next));
+                }
+                ind++;
+            }
+            */
         }
         return body;
     }
@@ -119,4 +206,16 @@ public class Parser {
         }       
         return words;
     }
+}
+class link{
+    public int pos;
+    public String title;
+    public String url;
+
+    public link(int pos, String title, String url) {
+        this.pos = pos;
+        this.title = title;
+        this.url = url;
+    }
+    
 }
